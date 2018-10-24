0e192602b3dea8ee5fc15e65376572081cc49e86
Josh Han <xhan91ca@gmail.com>
2018-10-24 14:05:03 -0700

    add new list ui

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,23 @@ import config from './firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
+import './plugins/iview.js';
 
 Vue.config.productionTip = false;
 
 firebase.initializeApp(config);
 
-const google = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(google).then((res) => {
-  store.dispatch('setUser', res);
-}).catch((err) => {
-  console.log(err);
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch('setUser', user);
+  }
 });
+
+// const google = new firebase.auth.GoogleAuthProvider();
+// firebase.auth().signInWithPopup(google).then((res) => {
+//   store.dispatch('setUser', res);
+// }).catch((err) => {});
 
 new Vue({
   router,
